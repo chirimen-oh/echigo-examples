3f351e4e98a9158e3216f03c8ec5ccc432d3f4ae
gurezo <akihiko.kigure@gmail.com>
2017-05-08 17:00:38 +0900

    Sakura IO Platfom Beta module coding.

---
+++ i2c-sakura-iot-platfom-beta/js/SakuraIO.js
@@ -0,0 +1,13 @@
+var SakuraIO = function(i2cPort,slaveAddress){
+  this.i2cPort = i2cPort;
+  this.slaveAddress = slaveAddress;
+};
+
+SakuraIO.prototype = {
+  read: function() {
+
+  },
+  write: function() {
+
+  }
+};
--- i2c-sakura-iot-platfom-beta/js/main.js
+++ i2c-sakura-iot-platfom-beta/js/main.js
@@ -6,14 +6,12 @@ window.addEventListener("load", function() {
   navigator.requestI2CAccess()
     .then(function(i2cAccess){
       var port = i2cAccess.ports.get(0);
-/*
-      var adt7410 = new ADT7410(port,0x48);
+      var sakuraio = new SakuraIO(port,0x48);
       setInterval(function(){
-        adt7410.read().then(function(value){
+        SakuraIO.read().then(function(value){
           console.log('value:', value);
           head.innerHTML = value ? value : head.innerHTML;
         });
       },1000);
-*/
     }).catch(e=> console.error('error', e));
 });
